70e014b09058729821b8514431835aaae9d7139b
maayanshp <mayyan01@gmail.com>
2024-12-31 11:15:44 +0200

    new files

---
+++ UserManagerAPI.py
@@ -0,0 +1,33 @@
+from flask import *
+
+app = Flask(__name__)
+
+# Create user
+#get data ,create user ,return json of user 
+@app.route('/users', methods=['POST'])
+def create_user():
+    return jsonify("user"), 201
+
+# Read user by ID
+#get id, return json of user  
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    return jsonify("user"), 200
+
+
+# Update user
+#get id and date , update user, return new json user
+@app.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    
+    return jsonify("new user"), 200
+
+# Delete user
+#get id, delete user, return message 
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    return 'User deleted'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
