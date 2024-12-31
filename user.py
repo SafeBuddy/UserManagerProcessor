c426011bf4c6557474aaa544a40141a49a7d1f0f
maayanshp <mayyan01@gmail.com>
2024-12-31 14:22:32 +0200

    add user properties

--- user.py
+++ user.py
@@ -1,1 +1,11 @@
-#hey
+class User:
+    def __init__(self, user_id, username,password,t_fullname,t_birthdate,t_phone,p_fullname,p_contact):
+        self.id = user_id
+        self.username = username
+        self.password=password
+        self.email = email
+        self.t_fullname=t_fullname
+        self.t_birthdate=t_birthdate
+        self.t_phone=t_phone
+        self.p_fullname=p_fullname
+        self.p_contact=p_contact
