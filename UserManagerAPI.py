dd81e1de798ce96d85e116ef84cd21824cbd73dc
rashilmbareky <116673155+rashilmbareky@users.noreply.github.com>
2025-01-05 03:22:41 +0200

    Creating user dictionary to save data and add to it initial data

--- UserManagerAPI.py
+++ UserManagerAPI.py
@@ -1,7 +1,35 @@
-from flask import *
+from flask import Flask, request, jsonify
+
 
 app = Flask(__name__)
 
+#In-memory user dictionary
+users_dict = {}
+user_id = 0
+
+#Add data for users_dict
+def initialize_users():
+    global user_id
+    init_users = [
+        {
+            "username": "stevewillson",
+            "password": "sw125689",
+            "t_fullname": "Steve John Willson",
+            "t_birthdate": "2012-02-03",
+            "t_phone": "0526547890",
+            "p_fullname": "John Fade Willson",
+            "p_contact": "0504655897",
+        },
+        {
+            "username": "katedoe",
+            "password": "kk078dd",
+            "t_fullname": "Kate Dan Doe",
+            "t_birthdate": "2010-05-10",
+            "t_phone": "0527700112",
+            "p_fullname": "Dan Joe Doe",
+            "p_contact": "0548869364",
+        }
+    ]
 # Create user
 #get data ,create user ,return json of user 
 @app.route('/users', methods=['POST'])
